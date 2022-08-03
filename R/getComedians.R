08b25b2fce47812a6497189fd39a94e12dc3f655
Daniel Vainberg <daniel.vainberg@mail.huji.ac.il>
2022-08-03 06:14:47 +0300

    stages split and uniqued, plus some comments and asthetics

--- R/getComedians.R
+++ R/getComedians.R
@@ -1,10 +1,16 @@
 #Importing and cleaning
 library(tidyverse)
-comics_raw <- read_csv("data/IsraeliComics2022_EDIT.csv",
+comics_raw <- read_csv("data/IsraeliComics2022_EDIT.csv", #edited file
                        col_names  = TRUE,
-                       col_types = "cfcff")
+                       col_types = "cfcff") #c = char; f = factor
 levels(comics_raw$sex) <- c("נקבה", "זכר")
-comics_clean <- comics_raw[!is.na(comics_raw$name),]
+comics_clean <- comics_raw[!is.na(comics_raw$name),] #fuck NA
+
+
+#splits stage into list of chr vectors, and finds unique values
+comics_clean$stages <- str_split(comics_clean$stage, pattern = ",")
+unique_stages <- unique(unlist(comics_clean$stages))
+
 
 #Analysis
 comics_clean %>% 
@@ -13,5 +19,6 @@ comics_clean %>%
 
 
 status_tab <- table(comics_clean$status)
-pie(status_tab[1:7], labels = paste0(names(status_tab), " (" ,round(status_tab/sum(status_tab)*100), "%)"))
-status_tab
+status_cent <- round(status_tab/sum(status_tab)*100)
+pie(status_tab[1:7], labels = paste0(names(status_tab), " (", status_cent, "%)"))
+
